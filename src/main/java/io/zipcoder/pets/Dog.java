e6f3d34e9a7f88892aa9afb63ec9ee8fa9041a8c
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-09 19:15:09 -0500

    created Pet subclasses

---
+++ src/main/java/io/zipcoder/pets/Dog.java
@@ -0,0 +1,8 @@
+package io.zipcoder.pets;
+
+public class Dog extends Pet{
+    @Override
+    public String speak(){
+        return "Woof!";
+    }
+}
---
+++ src/main/java/io/zipcoder/pets/Rabbit.java
@@ -0,0 +1,8 @@
+package io.zipcoder.pets;
+
+public class Rabbit extends Pet{
+    @Override
+    public String speak(){
+        return "Mmmm";
+    }
+}
