bae9d46984a9d1775ce3c483a71f1530dad70a09
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-09 19:12:17 -0500

    did pt1

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -12,13 +12,25 @@ public class MainApplication {
         System.out.println("Please enter how many pets you have: ");
         int numberOfPets = scanner.nextInt();
 
-        ArrayList<Pet> pets = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> types = new ArrayList<>();
+
+        scanner.nextLine();
 
         for(int i = 1; i <= numberOfPets; i++){
-            System.out.println("What is the name of Pet 1?: ");
+            System.out.println("What is the name of Pet "+i+"?: ");
             String name = scanner.nextLine();
+            names.add(name);
+
+            //scanner.nextLine();
+            System.out.println("What is the type of Pet "+i+"?: ");
+            String type = scanner.nextLine();
+            types.add(type);
 
         }
+
+        System.out.println(names);
+        System.out.println(types);
     }
 
 
